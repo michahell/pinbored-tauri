e2d70d39fab5f5c7c7433b004e0ceb6cf5274b61
Michael Trouw <michahell@soverin.net>
2026-03-26 11:43:49 +0100

    commit initial pnmp create tauri app scaffold

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { invoke } from "@tauri-apps/api/core";
+
+@Component({
+  selector: "app-root",
+  imports: [RouterOutlet],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
+})
+export class AppComponent {
+  greetingMessage = "";
+
+  greet(event: SubmitEvent, name: string): void {
+    event.preventDefault();
+
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    invoke<string>("greet", { name }).then((text) => {
+      this.greetingMessage = text;
+    });
+  }
+}
