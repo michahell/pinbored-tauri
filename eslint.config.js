78b8ba4967688d6e29d002b4a1a2ca4194779013
Michael Trouw <michahell@soverin.net>
2026-06-20 14:20:28 +0200

    remove sheriff from linting (not linters responsibility, hindsight)

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,6 @@ const eslint = require('@eslint/js')
 const tseslint = require('typescript-eslint')
 const angular = require('angular-eslint')
 const eslintConfigPrettier = require('eslint-config-prettier/flat')
-const sheriff = require('@softarc/eslint-plugin-sheriff')
 
 module.exports = defineConfig([
   {
@@ -40,9 +39,5 @@ module.exports = defineConfig([
     files: ['**/*.html'],
     extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
     rules: {},
-  },
-  {
-    files: ['**/*.ts'],
-    extends: [sheriff.configs.all],
   },
 ])
