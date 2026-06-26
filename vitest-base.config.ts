f48530fd4241fbdcfcc5d8125e19cedcec7a193e
Michael Trouw <michahell@soverin.net>
2026-06-26 22:31:46 +0200

    exclude src/libs from unit-test execution (and later also coverage)

--- vitest-base.config.ts
+++ vitest-base.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     isolate: true,
+    include: ['src/**/*.spec.ts'],
+    exclude: ['libs/**/*.spec.ts', '**/node_modules/**'],
   },
 })
